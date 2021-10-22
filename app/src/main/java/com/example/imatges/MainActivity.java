3b535f062274fdae865b016d37edbc75158dd32d
Spartanshooter <adriacostasalse@gmail.com>
2021-10-22 16:53:30 +0200

    hola

--- app/src/main/java/com/example/imatges/MainActivity.java
+++ app/src/main/java/com/example/imatges/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.imatges;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -16,9 +18,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -32,31 +31,56 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//       try {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        } catch (ActivityNotFoundException e) {
+//            //
+//        }
+//    }
+
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        final ImageView imageView = findViewById(R.id.imageView);
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            imageView.setImageBitmap(imageBitmap);
+//
+//            File path = getApplicationContext().getFilesDir();
+//            File foto = new File(path, "fofo.jpg");
+//            try {
+//                FileOutputStream os = new FileOutputStream(foto);
+//                imageBitmap.compress(Bitmap.CompressFormat.JPEG,100,os);//           } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+    protected File getfile() {
+        File path = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File foto = new File(path, "fofo.jpg");
+        return foto;
+    }
+    @SuppressLint("QueryPermissionsNeeded")
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            //
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            photoFile = getfile();
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         final ImageView imageView = findViewById(R.id.imageView);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-
-            File path = getApplicationContext().getFilesDir();
-            File foto = new File(path, "fofo.jpg");
-            try {
-                FileOutputStream os = new FileOutputStream(foto);
-                imageBitmap.compress(Bitmap.CompressFormat.JPEG,100,os);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
+        Uri fileUri = Uri.fromFile(getfile());
+        imageView.setImageURI(fileUri);
     }
 }
